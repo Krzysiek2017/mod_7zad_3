a63af7352d83b54ebc86611de6a125c2b7eaebbc
Krzysztof <musashimiyamoto@interia.eu>
2018-03-05 22:17:09 +0100

    mod_7zad_3

---
+++ redux-demo/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { createComment } from './actions';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+store.dispatch(createComment('First comment'));
+store.dispatch(createComment('Second comment'));
+
+registerServiceWorker();
