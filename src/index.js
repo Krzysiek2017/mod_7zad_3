ac07924fc66566846dd0e7e8908942e51ca4e314
Krzysztof <musashimiyamoto@interia.eu>
2018-03-06 17:03:09 +0100

    poprawki

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
 import reducer from "./reducer";
 import { createComment } from "./actions";
+import { createStore, combineReducers } from 'redux';
+import DevTools from './DevTools';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  DevTools.instrument()
+);
 
 ReactDOM.render(
   <Provider store={store}>
